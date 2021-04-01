129564eb197ffc5c39cb19f838fbd6a7ab924722
Cristian Chelu <cristianchelu@gmail.com>
2021-04-01 23:25:45 +0300

    server: open dev browser on start

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ npm install
 
 ## Running
 ```bash
+# Development ( http://localhost:3000/ )
+npm run dev:db
+npm run dev
+
 # Production
 npm run build
 NODE_ENV=production node src/server/index.js
-
-# Testing
-npm run dev:db
-npm run dev
 ```
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,13 @@
 const configureApp = require("./app");
 const config = require("./config");
+const open = require("open");
 
 async function startServer() {
     const app = await configureApp();
     app.listen(config.PORT);
+    if (config.NODE_ENV == "development") {
+        open(`http://localhost:${config.PORT}`);
+    }
 }
 
 startServer();
